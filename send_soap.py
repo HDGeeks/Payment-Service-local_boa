ff5d9e2a88d1ce0ed3e467aec3512bee7b17df96
Daniel Tesfai <dannyhd88@gmail.com>
2023-02-16 16:14:09 +0300

    retry added

--- send_soap.py
+++ send_soap.py
@@ -1,8 +1,17 @@
+from jinja2 import Environment, PackageLoader
 import requests
-url = "http://10.180.79.13:10001/payment/services/APIRequestMgrService"
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+#url = "http://10.180.79.13:10001/payment/services/APIRequestMgrService"
+url = "http://196.188.120.3:10001/payment/services/APIRequestMgrService"
 #headers = {'content-type': 'application/soap+xml'}
+# env = Environment(loader=PackageLoader('myapp', 'templates'))
+# template = env.get_template('soaprequests/WeatherSericeRequest.xml')
+# body = template.render()
 headers = {'content-type': 'text/xml'}
-body = """<?xml version="1.0" encoding="UTF-8"?>
+body = '''<?xml version="1.0" encoding="UTF-8"?>
             <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:api="http://cps.huawei.com/synccpsinterface/api_requestmgr" xmlns:req="http://cps.huawei.com/synccpsinterface/request" xmlns:com="http://cps.huawei.com/synccpsinterface/common">
    <soapenv:Header/>
    <soapenv:Body>
@@ -49,7 +58,20 @@ body = """<?xml version="1.0" encoding="UTF-8"?>
       </api:Request>
    </soapenv:Body>
 </soapenv:Envelope>
-			"""
+			'''
 
-response = requests.post(url, data=body, headers=headers)
-print(response.content)
+session = requests.Session()
+retry = Retry(connect=3, backoff_factor=0.5)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('http://', adapter)
+session.mount('https://', adapter)
+try:
+   session.post(url, data=body, headers=headers)
+except requests.exceptions.ConnectionError as e:
+   print(e)
+else:
+   print(session)
+
+
+
+
